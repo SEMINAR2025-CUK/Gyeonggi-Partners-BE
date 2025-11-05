7285619796f775ee4f2c00b9041ec6bd69c9ac92
yessh <keh1058@gmail.com>
2025-11-05 15:11:30 +0900

    Feat(Proposal): 솔루션 제안서 클래스 생성
    - Consenter : 동의자 정보 담긴 클래스
    - ContentFormat : JSONB로 사용될 제안서 내용 클래스
    - SubmitStatus : 제안서의 현재 상태 (제출 가능, 불가능, 투표중)

---
+++ src/main/java/org/example/gyeonggi_partners/domain/proposal/domain/model/Consenter.java
@@ -0,0 +1,11 @@
+package org.example.gyeonggi_partners.domain.proposal.domain.model;
+
+import lombok.Getter;
+
+@Getter
+public class Consenter {
+
+    private Long id;
+
+    private String nickname;
+}
