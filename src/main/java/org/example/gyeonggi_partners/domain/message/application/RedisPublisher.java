f5c6e5509cc8d2dbef01682404bf8ac8d61eddd6
jaemin0413 <95339052+jaemin0413@users.noreply.github.com>
2025-11-16 17:53:32 +0900

    [FIX] 메세지 역직렬화를 RedisSubscriber에서 하도록 수정

--- src/main/java/org/example/gyeonggi_partners/domain/message/application/RedisPublisher.java
+++ src/main/java/org/example/gyeonggi_partners/domain/message/application/RedisPublisher.java
@@ -1,11 +1,13 @@
 package org.example.gyeonggi_partners.domain.message.application;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.gyeonggi_partners.domain.message.api.dto.MessageRequest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RedisPublisher {
@@ -15,10 +17,15 @@ public class RedisPublisher {
     private final ChannelTopic topic;
 
     public void publish(MessageRequest request) {
+        
+        log.info("[RedisPublisher] 메시지 발행 시작 - channel: {}, roomId: {}, userId: {}, type: {}", 
+                topic.getTopic(), request.getRoomId(), request.getUserId(), request.getType());
 
         //ChannelTopic으로 설정된 채널에 메세지를 발행
         // convertAndSend는 주어진 객체를 직렬화기로 변환 후 지정한 토픽에 발행
         redisTemplate.convertAndSend(topic.getTopic(), request);
+        
+        log.info("[RedisPublisher] 메시지 발행 완료 - channel: {}", topic.getTopic());
     }
 
 
