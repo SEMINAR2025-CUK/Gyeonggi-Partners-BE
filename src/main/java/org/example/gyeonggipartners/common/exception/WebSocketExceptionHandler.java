f1e1a16bd52fb4cd6f42beba66cb0833f71f85b5
dlrlq <dlrlqor256@naver.com>
2025-11-25 20:20:47 +0900

    refactor: Exception Handler의 와일드카드 타입 제거
    - ApiResponse<?>를 ApiResponse<ErrorCode>로 구체화
    - GlobalExceptionHandler, WebSocketExceptionHandler 적용
    - SonarQube java:S1452 이슈 해결

--- src/main/java/org/example/gyeonggipartners/common/exception/WebSocketExceptionHandler.java
+++ src/main/java/org/example/gyeonggipartners/common/exception/WebSocketExceptionHandler.java
@@ -20,21 +20,21 @@ public class WebSocketExceptionHandler {
     // 사용자가 정의한 예외 chatException 발생시 이 핸들러로 캐치
     @MessageExceptionHandler(MessageException.class)
     @SendToUser("/queue/errors") // 유저 개인 에러 큐로 dto 전송
-    public ResponseEntity<ApiResponse<?>> handleMessageException(MessageException e) {
+    public ResponseEntity<ApiResponse<ErrorCode>> handleMessageException(MessageException e) {
         final ErrorCode errorCode = e.getErrorCode();
         log.error("handleMessageException: {}", errorCode.getMessage());
 
-        ApiResponse<?> response=ApiResponse.error(errorCode);
+        ApiResponse<ErrorCode> response=ApiResponse.error(errorCode);
         return new ResponseEntity<>(response,HttpStatusCode.valueOf(errorCode.getStatus()));
     }
 
     @MessageExceptionHandler(Exception.class)
     @SendToUser("/queue/errors")
-    public ResponseEntity<ApiResponse<?>> handleUnknownException(Exception e) {
+    public ResponseEntity<ApiResponse<ErrorCode>> handleUnknownException(Exception e) {
 
         log.error("handleUnhandledException: {}", e.getMessage());
         ErrorCode errorCode= MessageErrorCode.INTERNAL_SERVER_ERROR;
-        ApiResponse<?> response=ApiResponse.error(errorCode);
+        ApiResponse<ErrorCode> response=ApiResponse.error(errorCode);
 
         return new ResponseEntity<>(response,HttpStatusCode.valueOf(errorCode.getStatus()));
     }
