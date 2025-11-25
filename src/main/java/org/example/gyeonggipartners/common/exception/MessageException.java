cf667a401d2515249ccc7b4568982e49d539c28e
dlrlq <dlrlqor256@naver.com>
2025-11-25 20:35:37 +0900

    fix: ErrorCode 필드에 transient 키워드 추가
    - ErrorCode는 직렬화 불필요한 메타데이터이므로 transient 처리
    - 직렬화 시 불필요한 데이터 전송 방지 및 성능 개선
    - InvalidClassException 발생 가능성 제거

--- src/main/java/org/example/gyeonggipartners/common/exception/MessageException.java
+++ src/main/java/org/example/gyeonggipartners/common/exception/MessageException.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 public class MessageException extends RuntimeException {
 
     @Getter
-    private final ErrorCode errorCode;
+    private final transient ErrorCode errorCode;
     public MessageException(ErrorCode errorCode) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
